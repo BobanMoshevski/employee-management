733e03d499d1771d7280b0cc59037d2f1e5a5de8
moshe-boban <bmoshevski@gmail.com>
2026-05-01 22:18:55 +0200

    improve login view UI styling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Login</h1>
+  <div
+    class="border w-[95%] sm:w-[70%] md:w-[50%] lg:w-[30%] mx-auto rounded-lg text-white my-5 p-3"
+  >
+    <h2 class="text-3xl text-center font-semibold">Log in</h2>
+    <div class="mb-2">
+      <label class="block text-sm font-bold mb-2"> Email </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        class="focus:border-none focus:outline-none p-2 rounded bg-gray-500 w-[100%]"
+        placeholder="Email"
+      />
+    </div>
+    <div>
+      <label class="block text-sm font-bold mb-2"> Password </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        class="focus:border-none focus:outline-none p-2 rounded bg-gray-500 w-[100%]"
+        placeholder="Password"
+      />
+    </div>
+  </div>
 </template>
